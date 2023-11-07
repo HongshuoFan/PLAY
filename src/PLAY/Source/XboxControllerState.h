a14cf60fc538644b660974d79c56520a0b8528a6
Hongshuo Fan <54576889+HongshuoFan@users.noreply.github.com>
2023-11-07 22:39:45 +0000

    finished the xbox controller input

--- src/PLAY/Source/XboxControllerState.h
+++ src/PLAY/Source/XboxControllerState.h
@@ -10,6 +10,21 @@
 
 #pragma once
 
+#define XBOX_A                0x0100
+#define XBOX_B                0x0200
+
+#define XBOX_X                0x0800
+#define XBOX_Y                0x1000
+
+#define XBOX_LB               0x4000
+#define XBOX_RB               0x8000
+
+#define XBOX_LeftStick        0x0040
+#define XBOX_RightStick       0x0020
+
+#define XBOX_MENU             0x0800
+#define XBOX_VIEW             0x0400
+
 namespace XboxCotroller {
 
     typedef struct _AnalogStick {
@@ -35,8 +50,9 @@ namespace XboxCotroller {
         bool y;
         bool a;
         bool b;
-        bool select;
         bool menu;
+        bool view;
+        bool share;
         bool lb;
         bool rb;
 
--- src/PLAY/Source/XboxController_Input.cpp
+++ src/PLAY/Source/XboxController_Input.cpp
@@ -23,14 +23,81 @@ XboxController_Input::~XboxController_Input()
 {
 }
 
-void XboxController_Input::evaluateXboxCotrollerHidInputBuffer(uint8_t *_reportData) { 
-    // Convert sticks to signed range
+void XboxController_Input::evaluateXboxCotrollerHidInputBuffer(uint8_t *_reportData) {
+    
+    // Convert sticks bit to int
     XC_input.leftStick.x = (_reportData[2] << 8) | (_reportData[1] & 0xFF);
     XC_input.leftStick.y = (_reportData[4] << 8) | (_reportData[3] & 0xFF);
     XC_input.rightStick.x = (_reportData[6] << 8) | (_reportData[5] & 0xFF);
     XC_input.rightStick.y = (_reportData[8] << 8) | (_reportData[7] & 0xFF);
-//    XC_input.leftStick.x = ((uint16_t)_reportData[2] << 8) | _reportData[1];
-    std::cout<< XC_input.rightStick.y<<"\n";
+    
+    // Convert trigger to bit to int
+    XC_input.leftTrigger = (_reportData[10] << 8) | (_reportData[9] & 0xFF);
+    XC_input.rightTrigger = (_reportData[12] << 8) | (_reportData[11] & 0xFF);
+    
+    // Dpad
+   
+    int dpadState = static_cast<int>(_reportData[13]);
+    switch (dpadState){
+        case 0:
+            XC_input.dpad.up = false;
+            XC_input.dpad.down = false;
+            XC_input.dpad.right = false;
+            XC_input.dpad.left = false;
+            break;
+        case 1:
+            XC_input.dpad.up = true;
+            break;
+        case 2:
+            XC_input.dpad.up = true;
+            XC_input.dpad.right = true;
+            break;
+        case 3:
+            XC_input.dpad.right = true;
+            break;
+        case 4:
+            XC_input.dpad.right = true;
+            XC_input.dpad.down = true;
+            break;
+        case 5:
+            XC_input.dpad.down = true;
+            break;
+        case 6:
+            XC_input.dpad.left = true;
+            XC_input.dpad.down = true;
+            break;
+        case 7:
+            XC_input.dpad.left = true;
+            break;
+        case 8:
+            XC_input.dpad.left = true;
+            XC_input.dpad.up = true;
+            break;
+    }
+    
+    //buttons
+    
+    uint16_t buttonState = (_reportData[14] << 8) | _reportData[15];
+   
+    XC_input.buttons.a = (buttonState & XBOX_A) != 0;
+    XC_input.buttons.b = (buttonState & XBOX_B) != 0;
+    XC_input.buttons.y = (buttonState & XBOX_Y) != 0;
+    XC_input.buttons.x = (buttonState & XBOX_X) != 0;
+    
+    XC_input.buttons.rb = (buttonState & XBOX_RB) != 0;
+    XC_input.buttons.lb = (buttonState & XBOX_LB) != 0;
+    
+    XC_input.leftStick.stickPress =(buttonState & XBOX_LeftStick) != 0;
+    XC_input.rightStick.stickPress =(buttonState & XBOX_RightStick) != 0;
+    
+    uint16_t oButtons = (_reportData[15] << 8) | _reportData[16];
+    XC_input.buttons.view =(oButtons & XBOX_VIEW) != 0;
+    XC_input.buttons.menu =(oButtons & XBOX_MENU) != 0;
+    XC_input.buttons.share =(_reportData[16] & 0x00f) != 0;
+
+//    std::cout<< "rightStick" <<XC_input.rightStick.stickPress << std::endl;
+    
+  
 }
 
 
