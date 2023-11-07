88e43886258c375dba59dfc2c6d6ddad6bd84f6b
Hongshuo Fan <54576889+HongshuoFan@users.noreply.github.com>
2023-11-07 19:03:20 +0000

    working on xbox controller input

--- src/PLAY/Source/XboxController_Input.cpp
+++ src/PLAY/Source/XboxController_Input.cpp
@@ -25,8 +25,12 @@ XboxController_Input::~XboxController_Input()
 
 void XboxController_Input::evaluateXboxCotrollerHidInputBuffer(uint8_t *_reportData) { 
     // Convert sticks to signed range
-    XC_input.leftStick.x = (int)(_reportData[1]);
-    std::cout<< XC_input.leftStick.x<<"\n";
+    XC_input.leftStick.x = (_reportData[2] << 8) | (_reportData[1] & 0xFF);
+    XC_input.leftStick.y = (_reportData[4] << 8) | (_reportData[3] & 0xFF);
+    XC_input.rightStick.x = (_reportData[6] << 8) | (_reportData[5] & 0xFF);
+    XC_input.rightStick.y = (_reportData[8] << 8) | (_reportData[7] & 0xFF);
+//    XC_input.leftStick.x = ((uint16_t)_reportData[2] << 8) | _reportData[1];
+    std::cout<< XC_input.rightStick.y<<"\n";
 }
 
 
