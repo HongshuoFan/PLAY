c5159cc6a5a74401354ad5d96a2736fc526847eb
Hongshuo Fan <54576889+HongshuoFan@users.noreply.github.com>
2024-02-28 12:30:39 +0000

    change the JC OSC and MIDI output from the input

--- src/PLAY/Source/MainComponent.cpp
+++ src/PLAY/Source/MainComponent.cpp
@@ -255,8 +255,8 @@ void MainComponent::onJoyCon_L_DataReceived()
     {
         JC_input->evaluate_L_JC_HidInputBuffer(hidIO_1->reportData);
         JC_UI.l_jc_input = JC_input->l_jc_input;
-        osc_sender->send_L_JoyCon_OSC_message(JC_UI.l_jc_input, JC_UI.left_EnableStates);
-        midi_sender->send_L_JoyCon_MIDI_message(JC_UI.l_jc_input, JC_UI.left_EnableStates);
+        osc_sender->send_L_JoyCon_OSC_message(JC_input->l_jc_input, JC_UI.left_EnableStates);
+        midi_sender->send_L_JoyCon_MIDI_message(JC_input->l_jc_input, JC_UI.left_EnableStates);
     }
     
 }
@@ -268,8 +268,8 @@ void MainComponent::onJoyCon_R_DataReceived()
     {
         JC_input->evaluate_R_JC_HidInputBuffer(hidIO_2->reportData);
         JC_UI.r_jc_input = JC_input->r_jc_input;
-        osc_sender->send_R_JoyCon_OSC_message(JC_UI.r_jc_input, JC_UI.right_EnableStates);
-        midi_sender->send_R_JoyCon_MIDI_message(JC_UI.r_jc_input, JC_UI.right_EnableStates);
+        osc_sender->send_R_JoyCon_OSC_message(JC_input->r_jc_input, JC_UI.right_EnableStates);
+        midi_sender->send_R_JoyCon_MIDI_message(JC_input->r_jc_input, JC_UI.right_EnableStates);
     }
 }
 
